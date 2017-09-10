ce11693a1f3cae28b7d91c4382a3895a1168e810
Arseniy Pendryak <a.pendryak@yandex.ru>
2017-09-10 14:44:39 +0300

    simplify FileTreeCellRender

--- src/main/kotlin/com/warrior/jetbrains/test/ui/tree/FileTreeCellRender.kt
+++ src/main/kotlin/com/warrior/jetbrains/test/ui/tree/FileTreeCellRender.kt
@@ -4,16 +4,13 @@ import java.awt.Component
 import javax.swing.JLabel
 import javax.swing.JTree
 import javax.swing.tree.DefaultTreeCellRenderer
-import javax.swing.tree.TreeCellRenderer
 
-class FileTreeCellRender : TreeCellRenderer {
-
-    private val defaultTreeCellRender: DefaultTreeCellRenderer = DefaultTreeCellRenderer()
+class FileTreeCellRender : DefaultTreeCellRenderer() {
 
     override fun getTreeCellRendererComponent(tree: JTree, value: Any,
                                               selected: Boolean, expanded: Boolean,
                                               leaf: Boolean, row: Int, hasFocus: Boolean): Component {
-        val component = defaultTreeCellRender.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus)
+        val component = super.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus)
         val label = component as? JLabel ?: return component
         when (value) {
             is FileTreeNode -> label.icon = value.userObject.type.smallIcon
